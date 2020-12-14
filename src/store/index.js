4c5e52876eb01e0a15c7371690d24af10e44eb05
romakoch <romakoch2018@gmail.com>
2020-12-14 16:31:48 +0200

    Initial ToDo App

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+  	count: 0
   },
   mutations: {
+  	increment(state) {
+  		state.count++
+  	},
+  	decrement(state) {
+  		state.count--
+  	}
   },
   actions: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{this.$store.state.count}}</h1>
+    <button @click="increment">Increment +1</button>
+    <button @click="decrement">Increment -1</button>
   </div>
 </template>
+
+<script>
+	export default {
+		methods: {
+			increment() {
+				this.$store.commit('increment')
+    		console.log(this.$store.state.count)
+			},
+			decrement() {
+				this.$store.commit('decrement')
+    		console.log(this.$store.state.count)
+			}
+		}
+	}
+</script>
